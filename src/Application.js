5b6563c46983c59e67bf58a9c6e67daa4634cfa1
Cristian Echeverria <cristian.echeverri4@gmail.com>
2018-05-24 16:00:53 -0600

    Added Google Auth

--- src/Application.js
+++ src/Application.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { auth, database } from './firebase';
+import React, {Component} from 'react';
+import {auth, database} from './firebase';
 import CurrentUser from './CurrentUser';
 import SignIn from './SignIn';
 import NewRestaurant from './NewRestaurant';
@@ -7,16 +7,31 @@ import Restaurants from './Restaurants';
 import './Application.css';
 
 class Application extends Component {
-  constructor(props) {
-    super(props);
+  state = {
+    currentUser: null
+  }
+
+  componentDidMount() {
+    auth.onAuthStateChanged((currentUser) => {
+      this.setState({currentUser});
+    });
   }
 
   render() {
+    const {currentUser} = this.state;
     return (
       <div className="Application">
         <header className="Application--header">
           <h1>Lunch Rush</h1>
         </header>
+        <div>
+          {!currentUser && <SignIn/>}
+          {currentUser && <div>
+            <NewRestaurant/>
+            <CurrentUser user={currentUser}/>
+          </div>
+}
+        </div>
       </div>
     );
   }
--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase';
 
 const config = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: ""
+  apiKey: "AIzaSyC8DcPCKL7bLmHfKQiC8YQqQrwlkkKja3s",
+  authDomain: "tutor2go-react.firebaseapp.com",
+  databaseURL: "https://tutor2go-react.firebaseio.com",
+  projectId: "tutor2go-react",
+  storageBucket: "tutor2go-react.appspot.com",
+  messagingSenderId: "196396082810"
 };
 
 firebase.initializeApp(config);
